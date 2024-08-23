dbe0ab12834ab0001e26a4bf63269e142e9b88a5
leod306 <leo.dahlen@bobcats.gcsu.edu>
2024-08-23 11:01:07 -0400

    First change- everthing title case

--- Dahlen.py
+++ Dahlen.py
@@ -3,7 +3,7 @@
 print("Hello, World!")
 #make a new line asking the user their name
 name = input("What is your first name? ")
-# upper case the name
+# title case the name
 name = name.title()
 
 # print the name
