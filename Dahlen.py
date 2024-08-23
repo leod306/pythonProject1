74f24093d29a0e6083d7924de9aec3b8065b7318
Leo Dahlen <leodahlen@Leos-MacBook-Air-2.local>
2024-08-23 10:55:24 -0400

    Initial commit

---
+++ Dahlen.py
@@ -0,0 +1,11 @@
+# create a Hello world application
+# create a Hello world application
+print("Hello, World!")
+#make a new line asking the user their name
+name = input("What is your first name? ")
+# upper case the name
+name = name.upper()
+
+# print the name
+print("Hello, " + name + "!")
+
